774d0d3c537c48c13c24168fc17eb104afcf6499
Santiago Guinazu <santigui2003@gmail.com>
2024-02-16 10:29:35 -0300

    Front de mi ecommerce

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, TextField } from '@mui/material'
+import { Button, Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 export const LoginPage = () => {
@@ -8,24 +8,43 @@ export const LoginPage = () => {
     direction='column' 
     alignItems='center' 
     justifyContent='center' 
-    sx={{minHeight:'90vh'}}
+    sx={{minHeight:'90vh', backgroundColor: 'green'}}
     >
-      <Grid item>
-        <TextField type='email' 
-          label="Email" 
-          variant="outlined" 
-          size='small' 
-        />
-      </Grid>
-      <Grid item mt={1}>
-        <TextField type='password' 
-          label="Password" 
-          variant="outlined" 
-          size='small' 
-        />
-      </Grid>
-      <Grid item mt={1}>
-        <Button variant="outlined">Iniciar Sesion</Button>
+      <Grid item sx={{width: 450, backgroundColor: 'white', borderRadius:2, padding: 3}}>
+
+        <Typography variant='h4'>Login</Typography>
+
+        <Grid container>
+
+          <Grid item mt={3} xs={12}>
+            <TextField 
+              type='email' 
+              label="Email" 
+              variant="outlined" 
+              size='small' 
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item mt={1} xs={12}>
+            <TextField 
+              type='password' 
+              label="Password" 
+              variant="outlined" 
+              size='small' 
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item mt={3} xs={12}>
+            <Button variant="outlined" fullWidth>Iniciar Sesion</Button>
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end' mt={2}>
+            <a href="/auth/register">Registrarse</a>
+          </Grid>
+
+        </Grid>
       </Grid>
     </Grid>
   )
